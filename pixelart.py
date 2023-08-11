cf28821ae1b13f4db50d46f2b9f3d914c8ad3c82
itsmebombie <92307212+itsmebombie@users.noreply.github.com>
2023-08-11 14:22:14 +0300

    added sprite support and optimized transforms

--- pixelart.py
+++ pixelart.py
@@ -7,9 +7,12 @@ import os
 
 # ACTUAL CODE
 
-HEIGHT = 30
+HEIGHT = 32
 
-allowed_files = ["class.png", "club.png", "corridor.png", "residential.png"]
+# allowed_files = ["class.png", "club.png", "corridor.png", "residential.png"]
+
+# we exclude folders because spwn is slow as fuck with more than hundreds of kb in memory
+excluded_folders = ["cg", "menu", "poem_special", "stab"]
 
 dir = './ddlc-decompiled/images.rpa/images'
 outDir = './output.txt'
@@ -20,27 +23,44 @@ frs = {}
 
 for subdir, dirs, files in os.walk(dir):
     for file in files:
-        if file not in allowed_files: continue
+        # if file not in allowed_files: continue
 
-        img = cv2.imread(os.path.join(subdir, file))
+        # rhat the hell, this is supposed to exclude folders
+        continue_loop = False
+        for i in excluded_folders:
+            if i in subdir.replace(dir+os.sep, "").replace("\\", "/").split("/"): continue_loop = True; break 
+        if continue_loop: continue
+
+        img = cv2.imread(os.path.join(subdir, file), cv2.IMREAD_UNCHANGED)
         width, height, _ = img.shape
         ratio = height / width
         img = cv2.resize(img, (int(HEIGHT * ratio), HEIGHT), interpolation=cv2.INTER_AREA)
         width, height, _ = img.shape
-        frames[subdir.replace(dir+os.sep, "")+"/"+file] = img
+        frames[subdir.replace(dir+os.sep, "").replace("\\", "/")+"/"+file] = img
 
 for f in frames.keys():
-    frs[f] = []
-    for x in range(width):
-        frs[f].append([])
-        for y in range(height):
-            b, g, r = frames[f][x][y]
+    height, width, channels = frames[f].shape
+    frs[f] = {"res": [height, width], "image": []}
+
+    for x in range(height):
+        frs[f]["image"].append([])
+        for y in range(width):
+            b, g, r, a = 0, 0, 0, 0
+
+            # lmao good code
+            if channels == 4: b, g, r, a = frames[f][x][y]
+            elif channels == 3: 
+                b, g, r = frames[f][x][y]
+                a = 255
+            
             h, s, v = colorsys.rgb_to_hsv(r, g, b)
-            if v > 8:
-                frs[f][x].append([int(h*360), int(round(s, 3)*1000), int(round(v/255, 3)*1000)])
+            if a > 127:
+                frs[f]["image"][x].append([int(h*360), int(round(s, 3)*999), int(round(v/255, 3)*999)])
             else:
-                frs[f][x].append([])
-    print(f, " objects:", width*height, " h:", width, " w:", height)
+                frs[f]["image"][x].append([])
+
+    print(f, " objects:", width*height, " h:", height, " w:", width)
+
 
 with open(outDir, 'w') as f:
     f.write(str(frs).replace("'", '"').replace(" ", ""))
